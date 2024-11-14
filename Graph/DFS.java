94660b4f1996a002b1e39c5e9acda653fabcc1b9
Lavanya M <55652714+Lavanyamanohar11@users.noreply.github.com>
2024-11-14 12:41:04 +0530

    Update DFS.java

--- Graph/DFS.java
+++ Graph/DFS.java
@@ -1,6 +1,6 @@
 // https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
 
-class Solution {    
+class Solution {   
     public void dfs(int node, ArrayList<ArrayList<Integer>> adj, Set<Integer> visit, ArrayList<Integer> ans){
         ans.add(node);
         for(int neigh: adj.get(node)){
@@ -13,8 +13,8 @@ class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(ArrayList<ArrayList<Integer>> adj) {
         // Code here
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        Set<Integer> visit = new HashSet<Integer>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        Set<Integer> visit = new HashSet<>();
         visit.add(0);
         dfs(0, adj, visit, ans);
         return ans;
